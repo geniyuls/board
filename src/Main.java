ee48c1cbcdf669d74fe6ef3bbbba2f3bfff9b94d
geniyuls <yulisis123@naver.com>
2024-09-23 15:35:02 +0900

    maria_db_test

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,15 @@ public class Main {
                 String body = sc.nextLine();
 
                 int id = ++lastArticleId;
-                System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+
+                Article article = new Article();
+                article.id = id;
+                article.title = title;
+                article.body = body;
+
+                System.out.println("생성된 게시물 객체 : " + article);
+
+                System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
             }
 
             if (cmd.equals("exit")) {
@@ -35,3 +43,9 @@ public class Main {
         sc.close();
     }
 }
+
+class Article{
+    int id;
+    String title;
+    String body;
+        }
